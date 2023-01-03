557bcb9bd2abd1ade5ba3124fa8f5e8a332c9320
Isaac Eason <86130977+codethat01@users.noreply.github.com>
2023-01-03 22:31:52 +0000

    Create bubblesort.py

---
+++ Programs/Introduction/bubblesort.py
@@ -0,0 +1,10 @@
+def bubblesort(array):
+    swapped = True
+
+    while swapped:
+        swapped = False
+        
+        for index in range(len(array)-1):
+            if array[index] > array[index+1]:
+                array[index], array[index+1] = array[index+1], array[index]
+                swapped = True
