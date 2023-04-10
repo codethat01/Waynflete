cef50b6e731b42d766a1b59789115f70cde905e1
Isaac Eason <86130977+codethat01@users.noreply.github.com>
2023-04-10 16:55:47 +0100

    Update algorithm.py

--- Programs/Problem2/Algorithm2/algorithm.py
+++ Programs/Problem2/Algorithm2/algorithm.py
@@ -1,1 +1,13 @@
+def calculate_coefficient(n, k, coefficients):
+    coefficient = (k+1) * coefficients[n-1][k] + (n-k) * coefficients[n-1][k-1]
+    return coefficient
 
+  
+def generate_coefficients(max_n):
+    coefficients = [[1]+[0]*length for length in range(max_n+1)]
+
+    for n in range(1, max_n+1):
+        for k in range(1, n):
+            coefficients[n][k] = calculate_coefficient(n, k, coefficients)
+    
+    return coefficients
