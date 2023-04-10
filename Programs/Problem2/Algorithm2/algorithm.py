d014a6059215a97a75bea26625c8335596980d4e
Isaac Eason <86130977+codethat01@users.noreply.github.com>
2023-04-10 17:07:53 +0100

    Update algorithm.py

--- Programs/Problem2/Algorithm2/algorithm.py
+++ Programs/Problem2/Algorithm2/algorithm.py
@@ -4,10 +4,9 @@ def update_coefficient(n, k, coefficients):
   
 
 def generate_coefficients(max_n):
-    coefficients = [1]
+    coefficients = [1] + [0]*(max_n-1)
 
     for n in range(1, max_n+1):
-        coefficients.append(0)
         for k in range(n-1, 0, -1):
             update_coefficient(n, k, coefficients)
     
