f08f1630f09314dbed0af471960ba9bef33b9daf
Isaac Eason <86130977+codethat01@users.noreply.github.com>
2023-04-10 17:07:11 +0100

    Update user.py

--- Programs/Problem2/Algorithm2/user.py
+++ Programs/Problem2/Algorithm2/user.py
@@ -29,12 +29,11 @@ def update_coefficient(n, k, coefficients):
     coefficients[k] *= k+1
     coefficients[k] += (n-k) * coefficients[k-1] 
   
-  
+
 def generate_coefficients(max_n):
-    coefficients = [1]
+    coefficients = [1] + [0]*(max_n-1)
 
     for n in range(1, max_n+1):
-        coefficients.append(0)
         for k in range(n-1, 0, -1):
             update_coefficient(n, k, coefficients)
     
