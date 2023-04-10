a02dea0d812a626a3c2f19b065dc721f07634933
Isaac Eason <86130977+codethat01@users.noreply.github.com>
2023-04-10 16:55:28 +0100

    Update user.py

--- Programs/Problem2/Algorithm2/user.py
+++ Programs/Problem2/Algorithm2/user.py
@@ -1,1 +1,48 @@
+def format_term(power, coefficient):
+    string = ""
+    
+    if coefficient != 1 or power == 0:
+        string += str(coefficient)
 
+    if power > 0:
+        string += 'x'
+    if power > 1:
+        string += '^' + str(power)
+
+    return string
+
+  
+def print_polynomial(polynomial):
+    terms = []
+
+    for power, coefficient in enumerate(polynomial):
+        if coefficient != 0:
+            term = format_term(power, coefficient)
+            terms.append(term)
+
+    formatted_string = ' + '.join(terms[::-1])
+
+    print(f"A_{n}(x) = {formatted_string}")
+
+    
+def calculate_coefficient(n, k, coefficients):
+    coefficient = (k+1) * coefficients[n-1][k] + (n-k) * coefficients[n-1][k-1]
+    return coefficient
+
+  
+def generate_coefficients(max_n):
+    coefficients = [[1]+[0]*length for length in range(max_n+1)]
+
+    for n in range(1, max_n+1):
+        for k in range(1, n):
+            coefficients[n][k] = calculate_coefficient(n, k, coefficients)
+    
+    return coefficients
+
+
+
+if __name__ == "__main__":
+    
+    n = int(input())
+    coefficients = generate_coefficients(n)
+    print_polynomial(coefficients[n])
